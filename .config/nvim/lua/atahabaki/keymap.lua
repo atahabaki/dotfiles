c8b1cd4d385557cb26cf4f4a1751aae472f0bffa
A. Taha Baki <atahabaki@pm.me>
2022-06-21 01:31:29 +0300

    nvim: keymap open terminal v/h

--- .config/nvim/lua/atahabaki/keymap.lua
+++ .config/nvim/lua/atahabaki/keymap.lua
@@ -11,3 +11,6 @@ vim.g.maplocalleader = " "
 keymap("n", "<C-h>", "<C-w><C-h>", opts)
 -- escape terminal insert mode...
 keymap("t", "<Esc>", "<C-\\><C-n>", topts)
+-- open a terminal to
+keymap("n", "<leader>tv", ":vs | term<CR>", opts) -- the right
+keymap("n", "<leader>th", ":split | term<CR>", opts) -- the below
