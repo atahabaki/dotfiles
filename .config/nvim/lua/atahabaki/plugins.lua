0af4d430e9098dcc9858b2cb4fa3b76a73060353
A. Taha Baki <atahabaki@pm.me>
2022-06-26 02:19:31 +0300

    nvim: luasnip for snippets

--- .config/nvim/lua/atahabaki/plugins.lua
+++ .config/nvim/lua/atahabaki/plugins.lua
@@ -48,6 +48,11 @@ return packer.startup(function(use)
   use('hrsh7th/cmp-buffer')
   use('hrsh7th/cmp-path')
   use('hrsh7th/cmp-cmdline')
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- should be placed below of all plugins
