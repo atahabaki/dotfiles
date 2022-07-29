e41a2955f07735103ffde427c4be95fc803f0ed4
A. Taha Baki <atahabaki@pm.me>
2022-07-30 00:32:05 +0300

    nvim: lsp installer.on_server_ready is deprecated...

--- .config/nvim/lua/atahabaki/lsp/lsp-installer.lua
+++ .config/nvim/lua/atahabaki/lsp/lsp-installer.lua
@@ -3,35 +3,85 @@ if not status then
   return
 end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require("atahabaki.lsp.handlers").on_attach,
-		capabilities = require("atahabaki.lsp.handlers").capabilities,
-	}
+local servers = {
+  "sumneko_lua",
+  "dartls",
+  "pyright",
+  "rust_analyzer",
+  "taplo"
+}
 
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("atahabaki.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
+lsp_installer.setup()
 
-	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("atahabaki.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
+local status_ok, lspconfig = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
 
-	 if server.name == "pyright" then
-	 	local pyright_opts = require("atahabaki.lsp.settings.pyright")
-	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	 end
+local opts = {}
 
-	 if server.name == "rust-analyzer" then
-	 	local rust_analyzer_opts = require("atahabaki.lsp.settings.rust_analyzer")
-	 	opts = vim.tbl_deep_extend("force", rust_analyzer_opts, opts)
-	 end
+for _, server in pairs(servers) do
+  opts = {
+    on_attach = require("atahabaki.lsp.handlers").on_attach,
+    capabilities = require("atahabaki.lsp.handlers").capabilities,
+  }
 
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
+  if server == "jsonls" then
+    local jsonls_opts = require("atahabaki.lsp.settings.jsonls")
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  end
+
+  if server == "sumneko_lua" then
+    local sumneko_opts = require("atahabaki.lsp.settings.sumneko_lua")
+    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  end
+
+  if server == "pyright" then
+    local pyright_opts = require("atahabaki.lsp.settings.pyright")
+    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+  end
+
+  if server == "rust_analyzer" then
+    require('rust-tools').setup {
+      tools = { -- rust-tools options
+          autoSetHints = true,
+          hover_with_actions = true,
+          inlay_hints = {
+            show_parameter_hints = false,
+            parameter_hints_prefix = "",
+            other_hints_prefix = "",
+          },
+      },
+      server = {
+        settings = {
+          ["rust-analyzer"] = {
+            assist = {
+              importGranularity = "module",
+              importPrefix = "self",
+            },
+            checkOnSave = {
+              command = "clippy"
+            },
+            cargo = {
+              buildScripts = {
+                enable = true,
+              },
+              loadOutDirsFromCheck = true
+            },
+            procMacro = {
+              enable = true
+            },
+          }
+        }
+      }
+    }
+    goto continue
+    -- local rust_analyzer_opts = require("atahabaki.lsp.settings.rust_analyzer")
+    -- opts = vim.tbl_deep_extend("force", rust_analyzer_opts, opts)
+  end
+
+-- This scfunction is exactly the same as lspconfig's setup function.
+-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+lspconfig[server].setup(opts)
+::continue::
+end
